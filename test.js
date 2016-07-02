0f661d37ec30d56b676f1a171bb040aaac7d4ede
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-07-01 23:03:19 -0400

    Test file-format query

--- test.js
+++ test.js
@@ -24,18 +24,28 @@ test('gets all items and stops', function (t) {
 })
 
 test('works on datos.gob.mx', function (t) {
-  var total = 0
+  t.plan(2)
+
+  var total = {}
+
+  var counter = function (key) {
+    return through.obj(function (data, enc, next) {
+      total[key] = (total[key] || 0) + data.result.count
+      next()
+    })
+  }
 
   var searcher = ckan({ uri: 'http://datos.gob.mx/busca/api/' })
 
-  var stream = searcher.stream({fulltext: 'INEGI'})
-  stream.pipe(through.obj(function (data, enc, next) {
-    total += data.result.count
-    next()
-  }))
-
-  stream.on('end', function () {
-    t.true(total > 100, 'finds many datasets from INEGI')
-    t.end()
+  var s1 = searcher.stream({fulltext: 'INEGI'})
+  s1.on('end', function () {
+    t.true(total['INEGI'] > 100, 'finds many datasets from INEGI')
   })
+  s1.pipe(counter('INEGI'))
+
+  var s2 = searcher.stream({fulltext: 'res_format:GeoJSON'})
+  s2.on('end', function () {
+    t.true(total['res_format:GeoJSON'] >= 6, 'at least 6 GeoJSON datasets')
+  })
+  s2.pipe(counter('res_format:GeoJSON'))
 })
